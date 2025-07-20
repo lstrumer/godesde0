559fe160c89bd18b5b6baf3d10f5ba9671c93016
lstrumer <lstrumer@gmail.com>
2025-07-19 20:13:57 -0400

    Subiendo cambios

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"runtime"
+
+	"github.com/lstrumer/godesde0/variables"
+	_ "github.com/lstrumer/godesde0/variables"
 )
 
 func main() {
 	fmt.Println(runtime.GOOS)
+	fmt.Println(variables.Hola())
 }
---
+++ variables/mensajes.go
@@ -0,0 +1,5 @@
+package variables
+
+func Hola() string {
+	return "Lstrumer"
+}
